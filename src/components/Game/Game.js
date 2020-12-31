26f1fa141cbb678ad20ead9232f71a91627e3c92
satyasaibhushan <satyasaibhushan@gmail.com>
2021-01-01 02:59:28 +0530

    Added useCookie custom hook and used it

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,27 +1,53 @@
-import { useState, useRef } from "react";
+import { useState, useReducer } from "react";
 import Board from "../Board/Board.js";
 import ScoreBoard from "../ScoreBoard/ScoreBoard.js";
 import "./game.css";
 import { ReactComponent as Logo } from "../../assets/delete.svg";
+import useCookie from "../../useCookie";
+
+let initialState = { X: 0, O: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "O":
+      action.update(`{ "X":${state.X}, "O": ${state.O + 1} }`);
+      return { ...state, O: state.O + 1 };
+    case "X":
+      action.update(`{ "X":${state.X + 1}, "O": ${state.O} }`);
+      return { ...state, X: state.X + 1 };
+    case "reset":
+      action.update(`{ "X":0, "O": 0 }`);
+      return initialState;
+    default:
+      throw new Error();
+  }
+}
 
 const Game = () => {
-  const [wins, setWins] = useState({ X: 0, O: 0 });
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [cookie, updateCookie] = useCookie("noOfWins", '{ "X": 0, "O": 0 }');
+  const [state, dispatch] = useReducer(reducer, JSON.parse(cookie));
+  console.log(cookie, state, typeof(state) );
 
   return (
     <div className="Game">
       <Board
         wonGame={winner => {
           setTimeout(() => {
-            winner == "X" ? setWins({ ...wins, X: ++wins.X }) : setWins({ ...wins, O: ++wins.O });
+            winner == "X"
+              ? dispatch({ type: "X", update: updateCookie })
+              : dispatch({ type: "O", update: updateCookie });
           }, 500);
         }}
         changePlayer={player => setCurrentPlayer(player)}
       />
       <div className="ScroeBoardsContainer">
-        <ScoreBoard player="X" count={wins.X} isHighlight={currentPlayer == "X"} />
-        <ScoreBoard player="O" count={wins.O} isHighlight={currentPlayer == "O"} />
-        <div className="scoresResetDiv" data-tooltip="clear scores" onClick={() => setWins({ X: 0, O: 0 })}>
+        <ScoreBoard player="X" count={state.X} isHighlight={currentPlayer == "X"} />
+        <ScoreBoard player="O" count={state.O} isHighlight={currentPlayer == "O"} />
+        <div
+          className="scoresResetDiv"
+          data-tooltip="clear scores"
+          onClick={() => dispatch({ type: "reset", update: updateCookie })}>
           <Logo width="30px" height="30px" />
         </div>
       </div>
