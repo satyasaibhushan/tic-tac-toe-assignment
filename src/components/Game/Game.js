1d99280470d917491b84a822cdd064304db92912
satyasaibhushan <satyasaibhushan@gmail.com>
2020-12-31 16:34:24 +0530

    added styles to modal

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,20 +2,35 @@ import { useState, useReducer, useEffect } from "react";
 import Square from "../Square/Square";
 import checkWinner from "../../checkWinner";
 import Modal from "../Modal/Modal";
+import "./game.css";
 
 let n = 3;
-const initialState = isX => ({ values: Array(n * n).fill(null), isX, isModalOpen: false });
+let moves = 0;
+const initialState = isX => ({
+  values: Array(n * n).fill(null),
+  isX,
+  isModalOpen: false,
+  isGameOver: false,
+  winner: "",
+});
 
 let reducer = (state, action) => {
   switch (action.type) {
     case "clickedSquare":
+      moves++;
+      console.log(moves, "moves");
       return { ...state, values: action.values, isX: !state.isX };
     case "restart game":
+      moves = 0;
       return initialState(true);
     case "won":
-      return { ...state, isModalOpen: true };
+      return { ...state, isModalOpen: true, isGameOver: true, winner: action.winner };
     case "closeModal":
       return { ...state, isModalOpen: false };
+    case "draw":
+      return { ...state, isModalOpen: true, isGameOver: true, winner: "" };
+    // case "game over":
+    //   return { ...state, isGameOver: true };
     default:
       throw new Error();
   }
@@ -26,7 +41,7 @@ const Game = () => {
   const [state, dispatch] = useReducer(reducer, initialState(true));
 
   let clickedSquare = i => {
-    if (!state.values[i]) {
+    if (!state.values[i] && !state.isGameOver) {
       state.values[i] = state.isX ? "X" : "O";
       dispatch({ values: state.values, type: "clickedSquare" });
     }
@@ -39,6 +54,10 @@ const Game = () => {
       setTimeout(() => {
         dispatch({ type: "won", winner: !state.isX ? "X" : "O" });
       }, 500);
+    } else if (moves == n * n) {
+      setTimeout(() => {
+        dispatch({ type: "draw" });
+      }, 500);
     }
   };
 
@@ -50,7 +69,14 @@ const Game = () => {
   };
   return (
     <div>
-      <Modal isModalOpen={state.isModalOpen} closeModal={()=>dispatch({ type: "closeModal" })} />
+      <Modal
+        isModalOpen={state.isModalOpen}
+        text1={state.winner === "" ? "Draw!!" : "And the winner is!"}
+        winner={state.winner}
+        buttonText={"Restart Game"}
+        clickedRestart={() => dispatch({ type: "restart game" })}
+        closeModal={() => dispatch({ type: "closeModal" })}
+      />
       <div>Next Player is: {state.isX ? "X" : "O"}</div>
       <table className="gameContainer">
         <tbody>
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./modal.css";
+import { X, O } from "../../assets/svgs";
+
 const Modal = props => {
+  const winner = useRef(null);
   const divStyle = {
     display: props.isModalOpen ? "block" : "none",
   };
   function closeModal(e) {
-    e.stopPropagation();
+    e?e.stopPropagation():"";
     props.closeModal();
   }
+
+  useEffect(() => {
+    winner.current.innerHTML = props.winner == "X" ? X : props.winner == "O" ? O : "";
+  }, [props.winner]);
+
   return (
     <div className="modal" onClick={closeModal} style={divStyle}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
+      <div className="modal-content" style={{height:props.winner?"30%":"23%"}}onClick={e => e.stopPropagation()}>
         <span className="close" onClick={closeModal}>
           &times;
         </span>
+        <p style={{fontSize:props.winner?"27px":"42px"}}>{props.text1}</p>
+        <div ref={winner} className="winner"></div>
+        <div className="restartGameContainerModal" onClick={props.clickedButton}>
+          <button
+            onClick={() => {
+              closeModal();
+              props.clickedRestart();
+            }}>
+            {props.buttonText}
+          </button>
+        </div>
       </div>
     </div>
   );
